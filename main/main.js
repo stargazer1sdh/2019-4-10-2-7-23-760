7e1911992e28d1510745d7dd1eff61127f340608
sdh <251287012@qq.com>
2019-05-31 18:27:55 +0800

    o

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var parkprice = inputs.parkTime*0.25;
+	var price = 6;
+	if(inputs.distance>2 && inputs.distance<=8)
+		price += (inputs.distance-2)*0.8;
+	else if(inputs.distance>8){
+		price += 6*0.8+(inputs.distance-8)*1.2;
+	}
+	price += parkprice;
+    return Math.round(price);
 };
